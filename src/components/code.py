819bed5eb714a6c33a6f96524fe99e2c9ecf060a
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2024-01-01 19:44:10 +0000

    drop the bot.

--- src/components/code.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def display(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
--- src/components/constants.py
+++
@@ -1,4 +0,0 @@
-# Unique ID for bot instance -> int
-# Get one from https://t.me/BotFather
-
-BOT_ID = 0
